54b548951405444952c16f89a2578c02c0b8d850
Mayank27Jain <mayank.anushaktinagar@gmail.com>
2024-04-27 12:42:48 +0530

    Chapter 2: Templates and Jinja

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app # the variable from the package
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts) # Uses Jinja template engine on ./templates/index.html
